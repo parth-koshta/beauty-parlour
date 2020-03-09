5bd4d8f4cf650375c20267dd50fda40a9bf203b9
Parth Koshta <parth.k@theghack.in>
2020-03-09 11:29:05 +0530

    ui update

--- src/Screens/HomeScreen/index.js
+++ src/Screens/HomeScreen/index.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react";
-import "./styles.scss";
+import React, { Component } from 'react';
+import './styles.scss';
 
 export default class HomeScreen extends Component {
-  render() {
-    return (
-      <div>
-        <div className="banner-1">
-          <img
-            src={require("../../Assets/Images/back-1.jpg")}
-            className="banner-1-img"
-          />
+	render() {
+		return (
+			<div className="container">
+				<div className='banner-1'>
+					{/* <img
+						src={require('../../Assets/Images/back-2.jpg')}
+						className='banner-1-img'
+					/> */}
+					{/* <div className='banner-1-heading-wrapper'>
+						<h1 className='banner-1-heading'>
+							Unique Herbal Beauty Parlour
+						</h1>
+						<div
+							className='contact-us-btn-wrapper'
+							onClick={() => alert('contact us')}>
+							<a className='contact-us-btn'>Contact Us</a>
+						</div>
+					</div> */}
+				</div>
+        <div>
 
-          <div className="banner-1-heading-wrapper">
-            <h1 className="banner-1-heading">Unique Herbal Beauty Parlour</h1>
-            <div className="contact-us-btn-wrapper" onClick={()=>alert("contact us")}>
-                <a className="contact-us-btn">Contact Us</a>
-            </div>
-          </div>
         </div>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
